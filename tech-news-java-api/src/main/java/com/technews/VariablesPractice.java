e007f508afd596a7251a6065edfa7a1fdf7ce9dd
Victor Perez <zunaty@gmail.com>
2021-12-22 18:18:54 -0700

    Setting up models

---
+++ tech-news-java-api/src/main/java/com/technews/VariablesPractice.java
@@ -0,0 +1,15 @@
+package com.technews;
+
+public class VariablesPractice {
+    public static void main(String[] args) {
+        int x = 5;
+        double y = 8.8;
+        String b= "Book";
+
+        double sum = x + y;
+        String concat = x + b;
+
+        System.out.println(sum);
+        System.out.println(concat);
+    }
+}
